525558e0919f49d6019411c2d72f9f301e7bd856
angelasrsh <angelasrsh@gmail.com>
2025-07-29 19:13:56 -0500

    removed base in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,7 @@ import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
   site: YukinaConfig.site,
-  base: 'astro-blog-yukina',
+  base: '',
   integrations: [
     tailwind(),
     svelte(),
